ad09d5ac55e702e47a0b10815e724c01a4b3c9bc
Anurag <anuragsharma011011@gmail.com>
2022-07-05 14:13:13 +0530

    teapot

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 5000
 const app = express()
 
 app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/error', require('./routes/error'))
 
 app.listen(port ,()=>{
     console.log(`Server started on port ${port}`)
