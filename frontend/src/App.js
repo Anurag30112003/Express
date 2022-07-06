9af96260502ea0206629e6dfd2a0e5a87c801372
Anurag <anuragsharma011011@gmail.com>
2022-07-06 23:00:18 +0530

    Tailwindcss setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -49,6 +49,9 @@ function App() {
           >
             React Redux
           </a>
+          <h1 className="text-3xl bg-red-800 font-bold underline">
+      Hello world!
+    </h1>
         </span>
       </header>
     </div>
